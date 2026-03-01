0cc150aa397e17aa89b99d3154b30e9cabffcf91
aren-01 <arenaksy@gmail.com>
2026-02-28 20:00:57 -0500

    Update introduction.php

--- app/src/introduction.php
+++ app/src/introduction.php
@@ -1,10 +1,6 @@
 <span class="textstyle2"><b>Welcome to A+ Familty Healthcare data management software!</b></span>
 <span class="textstyle3">
 	<br/>
-	<br/>I am Lugal Aren Aksoy, and I created this containerized PHP application with the MySQL database for the AIT 620 course at Towson University.
-
-You can start by clicking some buttons on the menu on the left. The customers are displayed, and all the information in the cells is editable for both customers and scheduled appointments. Only the delete option is not available on the PHP application. Receptionists can add new customers and schedule new appointments. On the "create a report" page, they can download the appointment details as a PDF file with one click. Downloading the entire database is also possible in the "settings" page.<br />
-
-Important note: In a real case, the "verify insurance" option must verify the insurance ID by an external API from health insurance companies. However, for this assignment, I only created a button that makes the insurance row "verified" regardless of the validity.
+	<br/>APP is working!
 	<br/>
 </span>  
