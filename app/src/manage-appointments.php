0df954533bb99b502bd2bb05e3e3e06b9fa69ba0
aren-01 <arenaksy@gmail.com>
2026-02-28 20:01:28 -0500

    Update manage-appointments.php
    Update DB connection for AWS RDS

--- app/src/manage-appointments.php
+++ app/src/manage-appointments.php
@@ -1,10 +1,11 @@
 <?php
-$servername = "mysql";
-$username   = "root"; 
-$password   = "aplusfamily"; 
-$dbname     = "db_health";
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$dbname   = getenv('DB_NAME');
+$port     = getenv('DB_PORT');
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
